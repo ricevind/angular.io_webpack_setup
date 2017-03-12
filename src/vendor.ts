ca1c6026da0fc02d0f083d74b2ed8c74631c32b6
ricevind <ricevind@gmail.com>
2017-03-12 17:26:14 +0100

    added bootstrap imports and instructions to uncomment in vendor.ts

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,1 +1,8 @@
 //vendor librieries
+//vendor librieries
+// to add bootstrap with jquery
+// ----- npm install -S 4.0.0-alpha.6; and uncomment 
+// import 'bootstrap/scss/bootstrap.scss';
+// import 'script-loader!tether';
+// import 'script-loader!jquery/dist/jquery.js';
+// import 'script-loader!bootstrap/dist/js/bootstrap.js';
